acc523a0dbb9414146d99d01ab4ffeb512858c9a
clutchkingasiimov <saureyj43@gmail.com>
2022-03-03 22:49:40 +0100

    Restored Commit

---
+++ lcs.py
@@ -0,0 +1,19 @@
+#Computes the longest common subsequence of the algorithm
+def longest_common_subsequence(t1, t2):
+	# Calculate the length of the strings
+	m = len(t1)
+	n = len(t2)
+
+	# Dynamic programming approach for LCS
+	L = [[None] * (n + 1) for i in range(m + 1)]
+	for i in range(m + 1):
+		for j in range(n + 1):
+			if i == 0 or j == 0:
+				L[i][j] = 0
+			elif t1[i - 1] == t2[j - 1]:
+				L[i][j] = L[i - 1][j - 1] + 1
+			else:
+				L[i][j] = max(L[i - 1][j], L[i][j - 1])
+
+	# L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+	return L[m][n]
